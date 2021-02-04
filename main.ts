30097a369eeb3bb2e01b244c786dbafefaafc7db
Sehin <78458686+sehinlaw@users.noreply.github.com>
2021-02-04 00:29:01 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,11 +3,19 @@ basic.forever(function () {
     Degrees = input.compassHeading()
     if (Degrees < 45 || Degrees > 315) {
         basic.showString("N")
-    } else if (Degrees < 135) {
+    } else if (Degrees < 67.5) {
+        basic.showString("NE")
+    } else if (Degrees < 112.5) {
         basic.showString("E")
-    } else if (Degrees < 225) {
+    } else if (Degrees < 157.5) {
+        basic.showString("SE")
+    } else if (Degrees < 202.5) {
         basic.showString("S")
-    } else {
+    } else if (Degrees < 247.5) {
+        basic.showString("SW")
+    } else if (Degrees < 292.5) {
         basic.showString("W")
+    } else {
+        basic.showString("NW")
     }
 })
